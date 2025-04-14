9b58bd724d1420e701caec03922862658b350427
Ralph Delia <Ralphdelia1@gmail.com>
2025-04-14 14:12:46 -0400

    added a global css file, added css classes to allow multiple images that toggle between light and dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Frame",
+      customCss: ["./src/styles/global.css"],
       logo: {
         src: "./src/assets/frame-logo.svg",
       },
