07460470b382adcf288b31c9bf17d8b232ecd534
Mykel Murphy <murphy@akane.waseda.jp>
2025-04-15 16:22:16 -0400

    scaffold basic structure for splash page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import studiocmsUi from "@studiocms/ui";
 
 // https://astro.build/config
 export default defineConfig({
@@ -63,5 +64,6 @@ export default defineConfig({
         },
       ],
     }),
+    studiocmsUi(),
   ],
 });
