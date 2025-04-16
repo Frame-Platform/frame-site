67bccc407efd40fab01f21af5bd7b049e59d2336
Mykel Murphy <murphy@akane.waseda.jp>
2025-04-16 14:06:22 -0400

    make simple footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
       logo: {
         src: "./src/assets/frame-logo.svg",
       },
+      components: {
+        Footer: "./src/components/Footer.astro",
+      },
       social: [
         {
           icon: "github",
